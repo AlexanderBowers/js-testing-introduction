7224ccc883dc863507ea44140030644890e07648
AlexanderBowers <7319abowers@gmail.com>
2021-06-07 20:42:08 -0700

    struggling with end to end test

--- util.test.js
+++ util.test.js
@@ -1,3 +1,4 @@
+const puppeteer = require('puppeteer');
 const { generateText, checkAndGenerate } = require ('./util');
 
 test('should output name and age', () => {
@@ -16,3 +17,13 @@ test('should generate a valid text ouput', () => {
     const text = checkAndGenerate('Max', 29)
     expect(text).toBe('Max (29 years old)'); 
 })
+
+test('should click around', async () => {
+    const browser = await puppeteer.launch({
+        headless: false,
+        slowMo: 80,
+        args: ['--window-size=1920,1080']
+    });
+    const page = await browser.newPage();
+    await page.goto('file:///C:/Users/Alex/dev/js-testing-introduction/index.html');
+});
